d8a68e756f7cc8098b93c3c908d683cbfec6c350
yamamoto <yamamoto@kyms.jp>
2026-08-14 09:54:57 +0900

    fix: モバイルドキュメントメニューを標準化

--- docs/.vitepress/theme/SiteLayout.vue
+++ docs/.vitepress/theme/SiteLayout.vue
@@ -2,17 +2,11 @@
 import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import HomePage from './HomePage.vue'
-import DocsHeader from './DocsHeader.vue'
-import DocsSidebar from './DocsSidebar.vue'
 
 const route = useRoute()
 </script>
 
 <template>
   <HomePage v-if="route.path === '/'" />
-  <template v-else>
-    <DocsHeader />
-    <DocsSidebar />
-    <DefaultTheme.Layout />
-  </template>
+  <DefaultTheme.Layout v-else />
 </template>
